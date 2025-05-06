856d35db4c35b49882f6a697df0f9079f98feee8
DenBerez <dennis.m.berezin@gmail.com>
2025-05-05 21:33:38 -0400

    Spotify api

--- next.config.ts
+++ next.config.ts
@@ -25,8 +25,8 @@ const nextConfig: NextConfig = {
           {
             key: 'Content-Security-Policy',
             value: process.env.NODE_ENV === 'development'
-              ? "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; font-src 'self' data:; img-src 'self' data: https://source.unsplash.com https://picsum.photos https://*.picsum.photos https://cdn.weatherapi.com https://p.scdn.co https://upload.wikimedia.org; media-src 'self' https://p.scdn.co https://upload.wikimedia.org;"
-              : "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; font-src 'self' data:; img-src 'self' data: https://source.unsplash.com https://picsum.photos https://*.picsum.photos https://cdn.weatherapi.com https://p.scdn.co https://upload.wikimedia.org; media-src 'self' https://p.scdn.co https://upload.wikimedia.org;"
+              ? "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; font-src 'self' data:; img-src 'self' data: https://source.unsplash.com https://picsum.photos https://*.picsum.photos https://cdn.weatherapi.com https://p.scdn.co https://upload.wikimedia.org https://i.scdn.co; media-src 'self' https://p.scdn.co https://upload.wikimedia.org;"
+              : "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; font-src 'self' data:; img-src 'self' data: https://source.unsplash.com https://picsum.photos https://*.picsum.photos https://cdn.weatherapi.com https://p.scdn.co https://upload.wikimedia.org https://i.scdn.co; media-src 'self' https://p.scdn.co https://upload.wikimedia.org;"
           },
         ],
       },
