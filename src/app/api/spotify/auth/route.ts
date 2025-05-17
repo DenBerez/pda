6ccafece96e5f74a97b9cd3ed34ce43fb4c15496
DenBerez <dennis.m.berezin@gmail.com>
2025-05-16 22:48:32 -0400

    UX

--- src/app/api/spotify/auth/route.ts
+++ src/app/api/spotify/auth/route.ts
@@ -32,11 +32,7 @@ export async function GET(request: NextRequest) {
                 'user-read-currently-playing',
                 'user-read-playback-state',
                 'user-modify-playback-state',
-                'user-read-recently-played',
-                'audio-features',
-                'audio-analysis'
-
-
+                'user-read-recently-played'
             ].join(' ');
 
             // Generate a random state value for security
